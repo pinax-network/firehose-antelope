ba624ee3b3dc681aff3021c6bd7fa94bebe8018f
Matthieu Vachon <matt@streamingfast.io>
2022-04-06 00:34:15 -0400

    Updated default port to account for `firehose-arweave`

--- cmd/fireacme/cli/constants.go
+++ cmd/fireacme/cli/constants.go
@@ -6,16 +6,16 @@ const (
 
 	// Firehose chain specific port
 	//
-	// The initial 16XXX prefix is different for every chain supported by Firehose.
+	// The initial 17XXX prefix is different for every chain supported by Firehose.
 	// The current prefix is the one you should use for your chain. Once you have copied
 	// this whole repository, you should open a PR on firehose-acme to bump it again
-	// so the next team supporting Firehose will use 16XXXX and so forth.
-	MindreaderGRPCAddr           string = ":16010"
-	NodeManagerAPIAddr           string = ":16041"
-	MindreaderNodeManagerAPIAddr string = ":16009"
-	MergerServingAddr            string = ":16012"
-	RelayerServingAddr           string = ":16011"
-	FirehoseGRPCServingAddr      string = ":16042"
+	// so the next team supporting Firehose will use 17XXXX and so forth.
+	MindreaderGRPCAddr           string = ":17010"
+	NodeManagerAPIAddr           string = ":17041"
+	MindreaderNodeManagerAPIAddr string = ":17009"
+	MergerServingAddr            string = ":17012"
+	RelayerServingAddr           string = ":17011"
+	FirehoseGRPCServingAddr      string = ":17042"
 
 	// Data storage default locations
 	ATMDirectory         string = "file://{sf-data-dir}/atm"
