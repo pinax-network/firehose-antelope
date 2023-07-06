324eb56f1a7568cff3279f85090a90b4301b322a
Frederik Schöll <fschoell@posteo.de>
2023-07-06 17:14:17 +0200

    include action

--- codec/abi_decoder.go
+++ codec/abi_decoder.go
@@ -597,7 +597,7 @@ func (d *ABIDecoder) decodeAction(action *pbantelope.Action, globalSequence uint
 		return nil
 	}
 
-	zlog.Debug("decoded action data", zap.String("json_data", string(jsonData)))
+	zlog.Debug("successfully decoded action data", zap.String("action", action.SimpleName()), zap.String("json_data", string(jsonData)))
 	action.JsonData = string(jsonData)
 
 	return nil
