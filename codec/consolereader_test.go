d788041d55e3d800cc16d9e34f4213bd424b2a1d
Frederik Schöll <fschoell@posteo.de>
2023-09-12 12:37:18 +0200

    update dmlog on test cases

--- codec/consolereader_test.go
+++ codec/consolereader_test.go
@@ -85,7 +85,8 @@ func TestParseFromFile(t *testing.T) {
 		// readerOptions []ConsoleReaderOption
 	}{
 		// {"full", "testdata/deep-mind.dmlog", nil /*nil*/},
-		{"full-3.1.x", "testdata/deep-mind-3.1.x.dmlog", nil /*nil*/},
+		// {"full-3.1.x", "testdata/deep-mind-3.1.x.dmlog", nil /*nil*/},
+		{"dmlog", "testdata/dm.log", nil /*nil*/},
 		// {"max-console-log", "testdata/deep-mind.dmlog", blockWithConsole /*[]ConsoleReaderOption{LimitConsoleLength(10)}*/},
 	}
 
